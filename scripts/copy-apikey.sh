201852189233b9b2c64958620f78f348dccd8dee
loixe <134563259+loixe@users.noreply.github.com>
2023-07-31 16:19:33 -0400

    Update copy-apikey.sh

--- scripts/copy-apikey.sh
+++ scripts/copy-apikey.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "Copying IBM Cloud apikey into development environment..."
-# docker cp ~/.bluemix/apikey.json lab-bluemix-cf:/home/vscode 
-# docker exec lab-bluemix-cf sudo chown vscode:vscode /home/vscode/apikey.json
 docker cp ~/.bluemix/apikey.json vscode:/home/vscode 
 docker exec orders sudo chown vscode:vscode /home/vscode/apikey.json
 echo "Complete"
