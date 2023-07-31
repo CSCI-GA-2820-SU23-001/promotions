b0407de11fd82311ccf658f8ad80dcb54b179e8c
loixe <134563259+loixe@users.noreply.github.com>
2023-07-31 17:20:37 -0400

    Update promotions_steps.py

--- features/steps/promotions_steps.py
+++ features/steps/promotions_steps.py
@@ -6,27 +6,27 @@ Steps file for Pet.feature
 For information on Waiting until elements are present in the HTML see:
     https://selenium-python.readthedocs.io/waits.html
 """
-# import requests
-# from behave import given
+import requests
+from behave import given
 
-# # HTTP Return Codes
-# HTTP_200_OK = 200
-# HTTP_201_CREATED = 201
-# HTTP_204_NO_CONTENT = 204
+# HTTP Return Codes
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_204_NO_CONTENT = 204
 
-# @given('the following promotions')
-# def step_impl(context):
-#     """ Delete all Pets and load new ones """
+@given('the following promotions')
+def step_impl(context):
+    """ Delete all Pets and load new ones """
 
-#     # List all of the pets and delete them one by one
-#     rest_endpoint = f"{context.base_url}/promotions"
-#     context.resp = requests.get(rest_endpoint)
-#     assert(context.resp.status_code == HTTP_200_OK)
-#     for promo in context.resp.json():
-#         context.resp = requests.delete(f"{rest_endpoint}/{promo['id']}")
-#         assert(context.resp.status_code == HTTP_204_NO_CONTENT)
+    # List all of the pets and delete them one by one
+    rest_endpoint = f"{context.base_url}/promotions"
+    context.resp = requests.get(rest_endpoint)
+    assert(context.resp.status_code == HTTP_200_OK)
+    for promo in context.resp.json():
+        context.resp = requests.delete(f"{rest_endpoint}/{promo['id']}")
+        assert(context.resp.status_code == HTTP_204_NO_CONTENT)
 
-#     # load the database with new promotions
+    # load the database with new promotions
 #     for row in context.table:
 #         payload = {
 #             "name": row['name'],
